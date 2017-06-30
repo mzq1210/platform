e8f1dd769da4f4d6c8f9fd8e37af7f6f1b5d7096
mzq1210 <578377590@qq.com>
2017-06-30 16:35:18 +0800

    up

--- frontend/controllers/IndexController.php
+++ frontend/controllers/IndexController.php
@@ -24,29 +24,13 @@ class IndexController extends BaseController {
             $post = Yii::$app->request->get();
             $page = isset($post['page'])? $post['page'] : 0;
             $size = isset($post['size'])? $post['size'] : 10;
-            $data=Content::getContentList($page, $size);
-            foreach ($data as $key => $value){
-                $data[$key]['ctime'] = date('Y-m-d: H:i:s', $value['ctime']);
-                if($value['pic'] != ''){
-                    foreach (explode(',', $value['pic']) as $k => $v){
-                        $type = substr($v, strrpos($v, '.'));
-                        $data[$key]['pics'][$k] = $v.'_200x200'.$type;
-                    }
-                }
-            }
-            echo Json::encode($data);exit;
+            $data = Content::getContentList($page, $size);
+            $Content = $this->_optimizeData($data);
+            echo Json::encode($Content);exit;
         }
-        
-        $Content=Content::getContentList(0, 10);
-        foreach ($Content as $key => $value){
-            $Content[$key]['ctime'] = date('Y-m-d: H:i:s', $value['ctime']);
-            if($value['pic'] != ''){
-                foreach (explode(',', $value['pic']) as $k => $v){
-                    $type = substr($v, strrpos($v, '.'));
-                    $Content[$key]['pics'][$k] = $v.'_200x200'.$type;
-                }
-            }
-        }
+
+        $data = Content::getContentList(0, 10);
+        $Content = $this->_optimizeData($data);
         $config=$this->wxJsConfig();
         $category=Category::getCategoryData();
 
@@ -62,5 +46,55 @@ class IndexController extends BaseController {
         return $this->render('games');
     }
 
+    /**
+     * 处理数据
+     * @param $data
+     * @return mixed
+     */
+    private function _optimizeData($data){
+        foreach ($data as $key => $value){
+            $data[$key]['ctime'] = $this->_timeTran($value['ctime']);
+            if($value['pic'] != ''){
+                foreach (explode(',', $value['pic']) as $k => $v){
+                    $type = substr($v, strrpos($v, '.'));
+                    $data[$key]['pics'][$k] = $v.'_200x200'.$type;
+                }
+            }
+        }
+        return $data;
+    }
 
+    /**
+     * 处理时间
+     * @param $the_time
+     * @return bool|string
+     */
+    private function _timeTran($the_time) {
+        header("Content-type: text/html; charset=utf8");
+        date_default_timezone_set("Asia/Shanghai");   //设置时区
+        $now_time = date("Y-m-d H:i:s", time());
+        $now_time = strtotime($now_time);
+        $dur = $now_time - $the_time;
+        if ($dur < 0) {
+            return $the_time;
+        } else {
+            if ($dur < 60) {
+                return $dur . '秒前';
+            } else {
+                if ($dur < 3600) {
+                    return floor($dur / 60) . '分钟前';
+                } else {
+                    if ($dur < 86400) {
+                        return floor($dur / 3600) . '小时前';
+                    } else {
+                        if ($dur < 259200) {//3天内
+                            return floor($dur / 86400) . '天前';
+                        } else {
+                            return date("Y-m-d H:i:s", $the_time);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
--- frontend/views/index/index.php
+++ frontend/views/index/index.php
@@ -92,7 +92,7 @@ use yii\helpers\Url;
                 <div class="item-box">
                     <div class="font-12" style="color: #A1A2A4;padding: 8px 0;">
                         <img class="img-circle" src="<?php echo $value['headimgurl'];?>" style="width: 35px;height: 35px;">&nbsp;
-                        <?= $value['uname'];?><span><?php $value['ctime'];?></span>
+                        <?= $value['uname'];?><span style="margin-left: 10px;"><?= $value['ctime'];?></span>
                         <!--<div class="float-right top-tip" style="margin-top: 4px;">置顶</div>-->
                     </div>
                     <a href="<?php echo Url::toRoute(['/release/look','id' => $value['id']]);?>">
